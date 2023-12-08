f03e65f7950d825a0900fdc847c66d6e77d596c6
Judá Villalta <judavillalta@gmail.com>
2023-12-08 19:43:38 -0400

    Inital commit

---
+++ src/main/java/ui/HomePageUI.java
@@ -0,0 +1,14 @@
+package ui;
+
+import org.openqa.selenium.By;
+
+public class HomePageUI {
+    public static By loginButton = By.xpath("//div[@class='HPHeaderSignup']");
+    public static By signUpButton = By.xpath("//div[@class='HPHeaderSignup']");
+    public static By fullnameInput = By.xpath("//*[@id=\"ctl00_MainContent_SignupControl1_TextBoxFullName\"]");
+    public static By emailInput = By.xpath("//*[@id=\"ctl00_MainContent_SignupControl1_TextBoxEmail\"]");
+    public static By passwordInput = By.xpath("//*[@id=\"ctl00_MainContent_SignupControl1_TextBoxPassword\"]");
+    public static By signUpConfirm = By.xpath("//*[@id=\"ctl00_MainContent_SignupControl1_ButtonSignup\"]");
+    public static By loginEmailInput = By.xpath("//*[@id=\"ctl00_MainContent_LoginControl1_TextBoxEmail\"]");
+    public static By loginPassInput = By.xpath("//*[@id=\"ctl00_MainContent_LoginControl1_TextBoxPassword\"]");
+}
